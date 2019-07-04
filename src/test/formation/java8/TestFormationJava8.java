9a6fd9866da829294d05c9d10e8c7e67565e3a61
Sidaty <ksidatym@gmail.com>
2019-07-04 16:57:02 +0000

    Commit 05

--- src/test/formation/java8/TestFormationJava8.java
+++ src/test/formation/java8/TestFormationJava8.java
@@ -5,18 +5,10 @@
  */
 package test.formation.java8;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import java.util.StringJoiner;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.IntStream;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -28,18 +20,38 @@ public class TestFormationJava8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<String> noms = Arrays.asList("Toto78", "Tata", "Titi travaille");
-
+        List<String> noms = Arrays.asList("Toto78", "Tata", "Titi", "Titi travaille");
+        
         int tailleTotale = 0;
 
         long nb = noms
                 .stream()
-                .peek(nom -> System.out.println(nom))
-                .map(nom -> nom.length())
-                .peek(taille -> System.out.println(taille))
+                .peek(System.out::println)
+                .map(String::length)
+                .peek(System.out::println)
                 .filter(taile -> taile > 4)
-                .peek(taille -> System.out.println(taille))
+                .peek(System.out::println)
                 .count();
+
+        List<String> NOMS = noms
+                .stream()
+                .parallel()
+                .peek(System.out::println)
+                .map(String::toUpperCase)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+        
+        System.out.println(NOMS);
+
+        Map<String, List<String>> NOMS_P = noms
+                .stream()
+                .parallel()
+                .peek(System.out::println)
+                .map(String::toUpperCase)
+                .peek(System.out::println)
+                .collect(Collectors.groupingBy(nom -> nom.substring(1, 2)));
+        
+        System.out.println(NOMS_P);
 
         System.out.println("Count : " + nb);
 //        for (String nom : noms) {
@@ -49,6 +61,7 @@ public class TestFormationJava8 {
 //        }
 
         System.out.println("tailleTotale : " + tailleTotale);
+        
     }
 
 }
